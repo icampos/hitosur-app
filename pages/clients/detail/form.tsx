ecad0cc554268351ddfe3b10b9ae3be90da19082
Isaac Campos <isaac.camposcruz@gmail.com>
2022-07-26 15:01:14 -0600

    Changing schema

--- pages/clients/detail/form.tsx
+++ pages/clients/detail/form.tsx
@@ -136,7 +136,6 @@ export const ClientForm: React.FC<ClientFormProps> = ({
                 <input
                   type="text"
                   className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  defaultValue={client.address}
                   disabled={isDisabled}
                 />
               </div>
